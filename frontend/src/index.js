ff8c8e7d8eb06d2771a34172c8a1785a68e15571
guruprasadpanda <guruprasadpanda4646@gmail.com>
2026-04-05 15:02:30 +0530

    PWA added for web app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ✅ ADD
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// ✅ ADD THIS LINE
+serviceWorkerRegistration.register();
